abb04de8280046210700556aa393dbbcc0fc2bc3
Abhishek Paul <maddexter@Abhisheks-MacBook-Air.local>
2020-04-16 00:35:05 +0530

    Changes made for data storage in JSON and constant file

---
+++ classes/battleconstants.py
@@ -0,0 +1,7 @@
+
+
+WINNING_POINTS = 5
+LOSING_POINTS = 4
+MAX_LEVEL_POINTS = 10
+MAX_LEVEL = 4
+# Create more level enemies if you want to increase this value. Increase the value to more if you want to add more stages
